708d597f2287a0c9ce2adf14362004bb6564abe2
Pascal Vantrepote <pascal@tamajii.com>
2012-05-03 09:15:43 -0400

    AOP
    - Rename AOC to AOP
    - Start of the implementation

---
+++ FlexOC/AOP/Support/AOPProxy.h
@@ -0,0 +1,13 @@
+//
+//  AOPProxy.h
+//  FlexOC
+//
+//  Created by Pascal Vantrepote on 12-05-03.
+//  Copyright (c) 2012 Tamajii Inc. All rights reserved.
+//
+
+#import <FlexOC/Core/Proxy/LazyObjectProxy.h>
+
+@interface AOPProxy : LazyObjectProxy
+
+@end
---
+++ FlexOC/AOP/Support/AOPProxy.m
@@ -0,0 +1,13 @@
+//
+//  AOPProxy.m
+//  FlexOC
+//
+//  Created by Pascal Vantrepote on 12-05-03.
+//  Copyright (c) 2012 Tamajii Inc. All rights reserved.
+//
+
+#import "AOPProxy.h"
+
+@implementation AOPProxy
+
+@end
